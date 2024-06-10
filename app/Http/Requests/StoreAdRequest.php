08f840c8ed13684cdcbcc926ae4765cbb10beb6c
MrKekovich <mrkekovich.official@gmail.com>
2024-06-10 10:37:25 +0300

    refactor: Rename AdRequest.php to StoreAdRequest.php

--- app/Http/Requests/StoreAdRequest.php
+++ app/Http/Requests/StoreAdRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class AdRequest extends FormRequest
+class StoreAdRequest extends FormRequest
 {
     public function rules()
     {
