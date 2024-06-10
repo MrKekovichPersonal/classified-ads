2fa3b463b1e6abb89493710cc77886593b676049
MrKekovich <mrkekovich.official@gmail.com>
2024-06-10 10:39:56 +0300

    feat: Custom rules for StoreAdRequest.php

--- app/Http/Requests/StoreAdRequest.php
+++ app/Http/Requests/StoreAdRequest.php
@@ -9,10 +9,11 @@ class StoreAdRequest extends FormRequest
     public function rules()
     {
         return [
-            'title' => ['required'],
-            'description' => ['required'],
-            'images' => ['required'],
-            'price' => ['required', 'numeric'],
+            'title' => 'required|string|max:200',
+            'description' => 'nullable|string|max:1000',
+            'photos' => 'required|array|max:3',
+            'photos.*' => 'url',
+            'price' => 'required|numeric|min:0',
         ];
     }
 
