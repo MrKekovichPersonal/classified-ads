a813ec2fea5daa78d7f3d87d5034a86c0f4eabca
MrKekovich <mrkekovich.official@gmail.com>
2024-06-11 12:00:12 +0300

    fix: "z" index 50 for Navbar

--- resources/js/shared/ui/Navbar.vue
+++ resources/js/shared/ui/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="w-full flex justify-center items-center sticky top-0 border-b-2 border-neutral h-[65px] bg-base-100/95 backdrop-blur supports-[backdrop-filter]:bg-base-100/60">
+    class="h-[65px] z-50 flex justify-center items-center sticky top-0 border-b-2 border-neutral bg-base-100/95 backdrop-blur supports-[backdrop-filter]:bg-base-200/60">
     <div class="w-full container flex justify-center items-center">
       <h1 class="text-3xl">Classified Ads</h1>
     </div>
