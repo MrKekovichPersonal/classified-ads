2b180838d3068b357e402a4ecbb63ee60e844383
MrKekovich <mrkekovich.official@gmail.com>
2024-06-11 20:03:44 +0300

    refactor: Extract error handling logic into separate component

--- resources/js/pages/Ads.vue
+++ resources/js/pages/Ads.vue
@@ -2,21 +2,17 @@
 
 import Navbar from "@/shared/ui/Navbar.vue"
 import AdList from "@/widgets/Ad/AdGrid.vue"
-import ReloadIcon from "@/widgets/Ad/ReloadIcon.vue"
 
 import { useAdStore } from "@/entities/Ad/model"
 import { onMounted } from "vue"
 import { getAdQueryParams } from "@/entities/Ad"
+import Error from "@/shared/ui/Error.vue"
 
 const store = useAdStore()
 
-function handleReload() {
-  window.location.reload()
-}
-
 onMounted(() => {
-  const params = getAdQueryParams()
-  store.loadAds(params.page, params.sortBy, params.order)
+  const { page, sortBy, order } = getAdQueryParams()
+  store.loadAds(page, sortBy, order)
 })
 </script>
 
@@ -25,15 +21,5 @@ onMounted(() => {
   <div class="container mx-auto my-2">
     <AdList/>
   </div>
-  <div v-if="store.error"
-       class="fixed top-0 left-0 z-50 flex flex-col justify-center items-center w-screen h-screen bg-base-100/95 backdrop-blur supports-[backdrop-filter]:bg-base-200/60">
-    <h6>Error Occurred :(</h6>
-    <div class="bg-base-300 rounded-lg border border-neutral flex">
-      <h1 class="p-4 text-center text-3xl">{{ store.error }}</h1>
-      <div class="w-16 border-l border-neutral hover:bg-neutral cursor-pointer duration-300"
-           @click.prevent="handleReload">
-        <ReloadIcon class="w-full h-full p-3"/>
-      </div>
-    </div>
-  </div>
+  <Error v-if="store.error" :error-message="store.error"/>
 </template>
