cceababc3a5e74f30431098656dd7b8903e79fa1
MrKekovich <mrkekovich.official@gmail.com>
2024-06-11 15:06:45 +0300

    refactor: Reload instantly on "handleReload", new style for "Error occurred" header, loadAds on mount in Ads.vue

--- resources/js/pages/Ads.vue
+++ resources/js/pages/Ads.vue
@@ -5,20 +5,19 @@ import AdList from "@/widgets/Ad/AdGrid.vue"
 import ReloadIcon from "@/widgets/Ad/ReloadIcon.vue"
 
 import { useAdStore } from "@/entities/Ad/model"
+import { onMounted } from "vue"
+import { getAdQueryParams } from "@/entities/Ad"
 
 const store = useAdStore()
 
-
-let reloadCount = 0
 function handleReload() {
-  store.loadAds()
-  store.error = null
-  reloadCount++
-
-  if (reloadCount >= 3) {
-    window.location.reload()
-  }
+  window.location.reload()
 }
+
+onMounted(() => {
+  const params = getAdQueryParams()
+  store.loadAds(params.page, params.sortBy, params.order)
+})
 </script>
 
 <template>
@@ -30,8 +29,8 @@ function handleReload() {
        class="fixed top-0 left-0 z-50 flex flex-col justify-center items-center w-screen h-screen bg-base-100/95 backdrop-blur supports-[backdrop-filter]:bg-base-200/60">
     <h6>Error Occurred :(</h6>
     <div class="bg-base-300 rounded-lg border border-neutral flex">
-      <h1 class="ml-4 py-4 text-center text-3xl">{{ store.error }}</h1>
-      <div class="w-16 ml-4 border-l border-neutral hover:bg-neutral cursor-pointer duration-300"
+      <h1 class="p-4 text-center text-3xl">{{ store.error }}</h1>
+      <div class="w-16 border-l border-neutral hover:bg-neutral cursor-pointer duration-300"
            @click.prevent="handleReload">
         <ReloadIcon class="w-full h-full p-3"/>
       </div>
