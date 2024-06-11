851538bd9d3304af721a30381e90aeb5e4e51a07
MrKekovich <mrkekovich.official@gmail.com>
2024-06-11 11:30:21 +0300

    feat: Add "AdCard"

---
+++ resources/js/entities/Ad/ui/AdCard.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { TAd } from "@/entities/Ad/model"
+
+const { ad } = defineProps<{ ad: TAd }>()
+</script>
+
+<template>
+  <div class="card overflow-hidden border border-neutral rounded-lg bg-neutral shadow-md shadow-base-300 cursor-pointer hover:bg-base-300 duration-300">
+    <img :src="ad.main_photo" class="h-[200px] border-b border-neutral object-cover" :alt="ad.title"/>
+    <div class="p-4 card-body">
+      <h6 class="text-2xl font-bold card-title truncate">
+        {{
+          ad.title.length > 20
+            ? ad.title.slice(0, 20) + "..."
+            : ad.title
+        }}
+      </h6>
+      <p class="text-lg text-end">{{ ad.price }} €</p>
+    </div>
+  </div>
+</template>
