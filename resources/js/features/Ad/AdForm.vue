756cd290c636ffcc23d76e7e1063e2df58886ac1
MrKekovich <mrkekovich.official@gmail.com>
2024-06-12 10:00:38 +0300

    refactor: Use new input & textarea fields in AdForm + handle image error

--- resources/js/features/Ad/AdForm.vue
+++ resources/js/features/Ad/AdForm.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
 import { ref, watch } from "vue"
+import InputField from "@/shared/ui/InputField.vue"
+import TextAreaField from "@/shared/ui/TextAreaField.vue"
+import { handleImageError } from "@/shared/lib"
 
 const props = defineProps({
   show: {
@@ -7,6 +10,7 @@ const props = defineProps({
     default: false,
   },
 })
+
 const emits = defineEmits<{
   (e: "close"): void
   (e: "confirm", adData: { title: string, description: string, images: string[], price: number }): void
@@ -15,8 +19,8 @@ const emits = defineEmits<{
 const isVisible = ref(props.show)
 const title = ref("")
 const description = ref("")
-const imageUrls = ref<string[]>(["", "", ""]) // Инициализация с пустыми строками
-const price = ref<number | null>(null)
+const imageUrls = ref<string[]>(["", "", ""])
+const price = ref<number>(0)
 
 watch(() => props.show, (newVal) => {
   isVisible.value = newVal
@@ -26,7 +30,7 @@ function resetValues() {
   title.value = ""
   description.value = ""
   imageUrls.value = ["", "", ""]
-  price.value = 0.00
+  price.value = 0
 }
 
 function closeModal() {
@@ -35,48 +39,39 @@ function closeModal() {
 }
 
 function confirmAction() {
-  if (!price.value) return
-
   const adData = {
     title: title.value,
     description: description.value,
-    images: imageUrls.value.filter(url => url !== ""), // Отфильтровываем пустые строки
+    images: imageUrls.value.filter((url) => url !== ""),
     price: price.value,
   }
   emits("confirm", adData)
   resetValues()
 }
+
 </script>
 
 <template>
   <slot/>
-
   <div v-if="isVisible" class="modal modal-open">
     <div class="modal-box">
       <h3 class="font-bold text-lg">Create Ad</h3>
-      <form @submit.prevent="confirmAction">
-        <div class="mb-4">
-          <label class="block text-sm font-medium mb-1">Title</label>
-          <input type="text" v-model="title" class="input input-bordered w-full" required/>
-        </div>
-        <div class="mb-4">
-          <label class="block text-sm font-medium mb-1">Description</label>
-          <textarea v-model="description" class="textarea textarea-bordered w-full" required></textarea>
-        </div>
-        <div class="mb-4">
-          <label class="block text-sm font-medium mb-1">Image URLs (up to 3)</label>
-          <div v-for="(url, index) in imageUrls" :key="index" class="mb-2">
-            <input type="url" v-model="imageUrls[index]" class="input input-bordered w-full" placeholder="Image URL"/>
-            <img v-if="url" :src="url" alt="Preview" class="mt-2 max-h-48"/>
+      <form @submit.prevent="confirmAction" class="flex flex-col gap-2">
+        <InputField label="Title" v-model="title" required placeholder="Title"/>
+        <TextAreaField label="Description" v-model="description" required placeholder="Description"/>
+        <div>
+          <label class="block text-sm font-medium ">Image URLs (up to 3)</label>
+          <div v-for="(url, index) in imageUrls" :key="index">
+            <div class="my-3 flex flex-col justify-center">
+              <InputField type="url" v-model="imageUrls[index]" placeholder="Image URL"/>
+              <img v-if="url" :src="url" alt="Preview" class="mt-1 max-h-32 object-cover rounded-lg" @error="handleImageError"/>
+            </div>
           </div>
         </div>
-        <div class="mb-4">
-          <label class="block text-sm font-medium mb-1">Price</label>
-          <input type="number" step="0.01" v-model="price" class="input input-bordered w-full" required/>
-        </div>
+        <InputField label="Price" type="number" step="0.01" v-model="price" required placeholder="Price"/>
         <div class="modal-action">
-          <button type="submit" class="btn btn-primary">Submit</button>
           <button type="button" class="btn" @click="closeModal">Close</button>
+          <button type="submit" class="btn btn-primary">Submit</button>
         </div>
       </form>
     </div>
