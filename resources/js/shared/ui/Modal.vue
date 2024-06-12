ac638f123808b50b91ef676cccf0c86eff22878f
MrKekovich <mrkekovich.official@gmail.com>
2024-06-12 14:55:41 +0300

    feat: Add modal element

---
+++ resources/js/shared/ui/Modal.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+
+import { blurFilter } from "@/shared/lib/blurFilter"
+
+const props = defineProps({
+  show: {
+    type: Boolean,
+    default: false,
+  },
+  class: {
+    type: String,
+  }
+})
+
+</script>
+
+<template>
+  <slot name="trigger"/>
+  <dialog :open="props.show" class="modal bg-black/30" :class="props.class">
+    <slot name="content"/>
+  </dialog>
+</template>
+
+<style scoped>
+
+</style>
