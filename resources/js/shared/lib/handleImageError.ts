f41498efd6502ea148621091f5f2d4fae8013d74
MrKekovich <mrkekovich.official@gmail.com>
2024-06-12 19:50:43 +0300

    refactor: Remove logging statement in handleImageError.ts

--- resources/js/shared/lib/handleImageError.ts
+++ resources/js/shared/lib/handleImageError.ts
@@ -1,9 +1,9 @@
 export const defaultImage = "https://placehold.co/600x400/000/FFF?text=No+Image"
 
 export function handleImageError(event: Event) {
-  console.log("123")
   try {
     const target = event.target as HTMLImageElement
+    console.log(target.src)
     target.src = defaultImage
   } catch (e) {
     console.error(`Couldn't handle image error for event: ${event}`)
